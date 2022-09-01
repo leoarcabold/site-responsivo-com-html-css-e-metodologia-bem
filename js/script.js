de5e47c07bbfb2f6fda3b42e6dadbbdbb22ed50a
Leandro Soares <leoarcabold@gmail.com>
2022-09-01 15:44:44 -0300

    addicionado função para ocultar menu em tela pequena

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var alternador = document.querySelector('.js-botao-chaveador');
+var alternadorItem = document.querySelector('.js-menu');
 
-alternador.onclick = function(){
+alternador.onclick = function botao () {
     var menu = document.querySelector('.js-menu');
-    menu.classList.toggle("menu--exibindo");
+    menu.classList.toggle('menu--exibindo');
+}
+
+alternadorItem.onclick = function () {
+    var menuItem = document.querySelector('.js-menu');
+    menuItem.classList.toggle('menu--exibindo');
 }
